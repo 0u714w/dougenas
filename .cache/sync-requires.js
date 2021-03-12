1ebd8d41d2fce254a782bf8bce1a15aa6b2f90a4
Douglass Enas <douglass@Douglasss-MBP.localdomain>
2021-03-12 15:34:39 -0500

    services

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,8 +5,8 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/douglass/multiplydev/Retro/retro/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/douglass/multiplydev/Retro/retro/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/douglass/multiplydev/Retro/retro/src/pages/index.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/douglass/multiplydev/douglassenas/retro/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/douglass/multiplydev/douglassenas/retro/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/douglass/multiplydev/douglassenas/retro/src/pages/index.js")))
 }
 
